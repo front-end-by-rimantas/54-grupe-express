c8f2003957fa94766490ed9b062d1c5e7a766db1
Rimantas <belauzas@gmail.com>
2025-04-28 14:46:43 +0300

    Favicons and basic server routing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send(`
 <!DOCTYPE html>
@@ -20,12 +22,84 @@ app.get('/', (req, res) => {
 </head>
 <body>
     <h1>Labas rytas, Lietuva!</h1>
+    <script src="/js/main.js"></script>
 </body>
 </html>`);
 });
 
-app.get('/css/main.css', (req, res) => {
-    res.send('RESET CSS KODAS....');
+app.get('/about', (req, res) => {
+    res.send(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Express.JS</title>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Projekto pavadinimas" />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+    <h1>About</h1>
+    <script src="/js/main.js"></script>
+</body>
+</html>`);
+});
+
+app.get('/users/:userId', (req, res) => {
+    res.send(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Express.JS</title>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Projekto pavadinimas" />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+    <h1>User id: ${req.params.userId}</h1>
+    <script src="/js/main.js"></script>
+</body>
+</html>`);
+});
+
+app.get('*error', (req, res) => {
+    console.log(req.params);
+    console.log(req.params.error);
+
+    res.send(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Express.JS</title>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Projekto pavadinimas" />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+    <h1>404</h1>
+    <p>Page not found</p>
+    <p>:(</p>
+    <p>):</p>
+    <script src="/js/main.js"></script>
+</body>
+</html>`);
 });
 
 app.listen(port, () => {
