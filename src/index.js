3cea714f52cf176d0763b73737860af4553f204b
Rimantas <belauzas@gmail.com>
2025-04-30 13:31:45 +0300

    API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { pageHome } from './pages/home.js';
+import { count, pageHome, textList } from './pages/home.js';
 import { pageAbout } from './pages/about.js';
 import { pageUser } from './pages/user.js';
 import { page404 } from './pages/404.js';
@@ -11,6 +11,22 @@ app.use(express.static('public'));
 app.get('/', pageHome);
 app.get('/about', pageAbout);
 app.get('/users/:userId', pageUser);
+
+app.get('/api/add-text/:text', (req, res) => {
+    textList.push(req.params.text.replaceAll('-', ' '));
+    res.send('Labas ir tau');
+});
+
+app.get('/api/update-count/:number', (req, res) => {
+    const n = +req.params.number;
+
+    if (!isNaN(n)) {
+        count.value = n;
+    }
+
+    res.send('Gautas skaicius: ' + n);
+});
+
 app.get('*error', page404);
 
 app.listen(port, () => {
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,41 @@
 import { pageTitle } from "../components/pageTitle.js";
 import { baseTemplate } from "../templates/baseTemplate.js";
 
+export const textList = [
+    'Lorem ipsum dolor sit amet 1.',
+    'Lorem ipsum dolor sit amet 2.',
+    'Lorem ipsum dolor sit amet 3.',
+    'Lorem ipsum dolor sit amet 4.',
+    'Lorem ipsum dolor sit amet 5.',
+];
+export let count = {
+    value: 0,
+};
+
 export function pageHome(req, res) {
+    count.value++;
+
+    let pHTML = '';
+
+    for (const text of textList) {
+        pHTML += `<p class="col-12 col-md-6 col-lg-4 col-xl-3 block">${text}</p>`;
+    }
+
     res.send(baseTemplate('', 0, `
         ${pageTitle('Labas rytas, Lietuva!')}
+        <section class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div style="font-size: 5rem;">Puslapio peržiūros: ${count.value}</div>
+                </div>
+            </div>
+        </section>
+        <section class="container bg-grey">
+            <div class="row">
+                <h2 class="col-12">Tekstai</h2>
+            </div>
+            <div class="row">${pHTML}</div>
+        </section>
         ${pageTitle('Labas rytas, Lietuva!')}
         ${pageTitle('Labas rytas, Lietuva!')}
         ${pageTitle('Labas rytas, Lietuva!')}
