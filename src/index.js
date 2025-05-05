f290f836119644d916f301a8b61d99ce2b4d7d21
Rimantas <belauzas@gmail.com>
2025-05-05 13:39:55 +0300

    Updatee

--- src/index.js
+++ src/index.js
@@ -9,24 +9,78 @@ const port = 3000;
 // kodas, kur padeda gauti POST duomenis?????
 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', pageHome);
 app.get('/about', pageAbout);
 app.get('/users/:userId', pageUser);
 
 app.all('/api', (req, res) => {
-    res.status(404).send('Toks adresas negalimas');
+    return res.status(404).send('Toks adresas negalimas');
 });
 
 app.post('/api/text', (req, res) => {
-    console.log(req.body);
+    textList.push(req.body.text);
 
-    // textList.push();
-    res.send('Sukurtas naujas tekstas');
+    return res.json({
+        status: 'success',
+        msg: 'Sukurtas naujas tekstas',
+    });
 });
-app.put('/api/text/:index/:text', (req, res) => {
-    textList[req.params.index] = req.params.text.replaceAll('-', ' ');
-    res.send('Tekstas sekmingai atnaujintas');
+
+app.put('/api/text/:index', (req, res) => {
+    const index = parseInt(req.params.index);
+
+    if (isNaN(index) || req.params.index !== '' + index) {
+        return res.json({
+            status: 'error',
+            msg: 'Index turi buti sveikasis skaicius',
+        });
+    }
+
+    if (index < 0) {
+        return res.json({
+            status: 'error',
+            msg: 'Index negali buti neigiamas',
+        });
+    }
+
+    if (index > textList.length) {
+        return res.json({
+            status: 'error',
+            msg: 'Per didelis index\'as',
+        });
+    }
+
+    if (typeof req.body !== 'object'
+        || Array.isArray(req.body)
+        || null) {
+        return res.json({
+            status: 'error',
+            msg: 'Duomenys turi buti objekto strukturos',
+        });
+    }
+
+    if (Object.keys(req.body).length !== 1 || !('text' in req.body)) {
+        return res.json({
+            status: 'error',
+            msg: 'Duomenu objektas turi tureti raktus: text',
+        });
+    }
+
+    if (typeof req.body.text !== 'string') {
+        return res.json({
+            status: 'error',
+            msg: 'Rakto "text" reiksme turi buti tekstine',
+        });
+    }
+
+    textList[index] = req.body.text;
+
+    return res.json({
+        status: 'success',
+        msg: 'Tekstas sekmingai atnaujintas',
+    });
 });
 
 app.put('/api/update-count/:number', (req, res) => {
@@ -34,7 +88,7 @@ app.put('/api/update-count/:number', (req, res) => {
     if (!isNaN(n)) {
         count.value = n;
     }
-    res.send('Gautas skaicius: ' + n);
+    return res.send('Gautas skaicius: ' + n);
 });
 
 app.get('*error', page404);
